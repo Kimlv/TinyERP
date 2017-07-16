7aea1983032d005f371202e510a284fa62d2d5ff
Tu Tran <contact@tranthanhtu.vn>
2017-07-16 16:49:41 +0700

    apply remote event

---
+++ api/App.Customer/Api/CustomerHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace App.Customer.Api
+{
+    using App.Common.DI;
+    using App.Common.Event;
+    using App.Common.Logging;
+    using App.Order.Event;
+    using System.Web.Http;
+
+    [RoutePrefix("api/customers")]
+    public class CustomerHandler : RemoteEventSubcriberHandler
+    {
+        [Route("onOrderCreated")]
+        [HttpPost]
+        public void OnOrderCreated(OnOrderCreated ev)
+        {
+            ILogger logger = IoC.Container.Resolve<ILogger>();
+            logger.Info("new order created, id:{0}", ev.OrderId);
+        }
+
+    }
+}
---
+++ api/Application.Common/Tasks/EventSubcriber/RegisterRemoteEventSubcriberTask.cs
@@ -0,0 +1,14 @@
+﻿namespace App.Common.Tasks.EventSubcriber
+{
+    public class RegisterRemoteEventSubcriberTask : BaseTask<TaskArgument<System.Web.HttpApplication>>, IApplicationStartedTask<TaskArgument<System.Web.HttpApplication>>
+    {
+        public RegisterRemoteEventSubcriberTask() : base(ApplicationType.All)
+        {
+        }
+
+        public override void Execute(TaskArgument<System.Web.HttpApplication> arg)
+        {
+            if (!this.IsValid(arg.Type)) { return; }
+        }
+    }
+}
