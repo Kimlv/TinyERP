a83472bfc621b0ce8fd84886a175736f49034185
Tu Tran <contact@tranthanhtu.vn>
2017-10-11 14:10:16 +0700

    Deplloy to azure

---
+++ api/Application.Api/Features/Common/HelloController.cs
@@ -0,0 +1,12 @@
+﻿namespace App.Api.Features.Common
+{
+    using System.Web.Http;
+    [RoutePrefix("api/hello")]
+    public class HelloController : ApiController
+    {
+        [Route("")]
+        public string SayHello() {
+            return "Hello";
+        }
+    }
+}
