62cd787979915cbe90515cd21d2ff724d2abffb9
Tu Tran <contact@tranthanhtu.vn>
2017-10-13 18:34:24 +0700

    Deploy to azure using visual studio

---
+++ api/Application.Api/Features/Common/UtilitiesController.cs
@@ -0,0 +1,16 @@
+﻿namespace App.Api.Features.Common
+{
+    using App.Common.MVC;
+    using App.Common.MVC.Attributes;
+    using System.Web.Http;
+    [RoutePrefix("api/utilities")]
+    public class UtilitiessController : BaseApiController
+    {
+        [HttpGet]
+        [Route("sayHello")]
+        [ResponseWrapper()]
+        public string SayHello() {
+            return "Hello";
+        }
+    }
+}
