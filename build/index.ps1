5163a99a6c4ad912fe86486078fb44b87e019247
Tu Tran <contact@tranthanhtu.vn>
2017-10-31 17:14:41 +0700

    Publish scproj project

--- build/index.ps1
+++ build/index.ps1
@@ -2,20 +2,13 @@
 # we need to install:
 # PSFTP module
 
-$global:msbuildPath="C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
-$msbuildPath="C:\Windows\Microsoft.NET\Framework64\v3.5\MSBuild.exe"
+$Global:msbuildPath="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
 $root=Resolve-Path ../
 $outputDir="$root\deployment"
 $Global:logger = $null
 $Global:FolderSeparator="\"
 $Global:FtpFolderSeparator="/"
-# Add-Type -TypeDefinition @"
-# 	public enum ActionStatusType{
-# 		None=0,
-# 		Success=200,
-# 		Fail=-1
-# 	}
-# "@
+$Global:FtpTimeout = 1000
 Enum AppStatusCode{
 	Success
 	Error
@@ -23,6 +16,7 @@ Enum AppStatusCode{
 Enum ActionStatusType{
 	None
 	Success
+	Exists
 	Fail
 }
 
@@ -64,89 +58,50 @@ Class BuildAgent{
 	#	$this.UploadToRemoteHost()
 	}
 	BuildProject(){
-		Write-Host "Starting building '$($this.FileToBuild)' ..."
-		Invoke-Expression "$($global:msbuildPath) $($this.FileToBuild) /p:OutputPath=$($this.OutputFolder) /p:PublishProfile=FolderProfile"
-		Write-Host "Building '$($this.FileToBuild)' was completed..."
+		$GLobal:logger.Write("Starting building '$($this.FileToBuild)' ...")
+		#$arguments = @()
+		#$arguments+=('$this.FileToBuild', "/p:DeployOnBuild=true",  "/p:OutputPath=$($this.OutputFolder)", "/p:PublishProfile=FolderProfile")
+		#Invoke-Expression "& '$Global:msbuildPath' $arguments"
+		Invoke-Expression "& '$($Global:msbuildPath)' $this.FileToBuild /p:DeployOnBuild=true  /p:OutputPath=$($this.OutputFolder) /p:PublishProfile=FolderProfile "
+		$GLobal:logger.Write("Building '$($this.FileToBuild)' was completed...")
 	}
 	UploadArtifactsToRemote(){
 		# Config
-		$Username = 'tinyerp\$tinyerp'
+		$UserName = 'tinyerp\$tinyerp'
 		$Password = "2rFnjrrgKFp6bDsz1xjE35M8RaN8btlpDAfg39Eugi3s7zmdl3qSf2gqxTj1"
-		$LocalFile = "D:\project\tfs_tinyerp\deployment\webapi\webapi.zip"
-		$RemoteFolder = "ftp://waws-prod-sn1-113.ftp.azurewebsites.windows.net/site/wwwroot/"
+		#$LocalFile = "D:\project\tfs_tinyerp\deployment\webapi\New_folder\ThoughtWorks.Selenium.Core.dll"
+		$RemoteFolder = "ftp://waws-prod-sn1-113.ftp.azurewebsites.windows.net/site/wwwroot"
+		
+		[FTP] $ftp = [FTP]::new($this.OutputFolder, $RemoteFolder, $UserName, $Password)
 		$files = [FileHelper]::GetAllFiles($this.OutputFolder)
-		[FTP] $ftp = [FTP]::new($this.OutputFolder, $RemoteFolder, $Username, $Password)
-
 		ForEach($file in $files){
 			$relativePath=$file.replace($this.OutputFolder + "\",'')
-			$Global:logger.Write("Working with '$($relativePath)'...")
 			if([FileHelper]::ContainFolder($relativePath) -and ($ftp.CreateFolder([System.IO.Path]::GetDirectoryName($relativePath)).Status -eq [ActionStatusType]::Fail)){
 				$Global:logger.Error("Error while working on '$($relativePath)'. Please check in the console for more information.")
 				Exit [AppStatusCode]::Error
-				
 			}
 
-			$remoteFile = $RemoteFolder + $relativePath
+			#$remoteFile = $RemoteFolder + $relativePath
 			$Global:logger.Write("Start uploading '$($relativePath)'...")
-			#Write-Host "Start uploading '$($relativePath)' to $($remoteFile) ..."
 			$ftp.Upload($relativePath)
-			#$ftp.Upload($file, $remoteFile, $Username, $Password)
 			$Global:logger.Write("'$($relativePath)' was uploaded.")
-			# Write-Host "'$($relativePath)' was uploaded."
 		}
-		# $relativePath=$localFile.replace($this.OutputFolder,'')
-		# Write-Host "Start uploading '$($relativePath)' ..."
-		# [FTPHelper]::Upload($LocalFile, $RemoteFile, $Username, $Password)
-		# Write-Host "'$($relativePath)' was uploaded."
-		# Create a FTPWebRequest 
-		# $FTPRequest = [System.Net.FtpWebRequest]::Create($RemoteFile) 
-		# $FTPRequest.Credentials = New-Object System.Net.NetworkCredential($Username,$Password) 
-		# $FTPRequest.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile 
-		# $FTPRequest.UseBinary = $true 
-		# $FTPRequest.KeepAlive = $false
-		# #$FTPRequest.UsePassive = $true
-		# # Send the ftp request
-		# $FTPResponse = $FTPRequest.GetResponse() 
-		# # Get a download stream from the server response 
-		# $ResponseStream = $FTPResponse.GetResponseStream() 
-		# # $files = [FileHelper]::GetAllFiles($this.OutputFolder)
-		# # ForEach($file in $files){
-		# # 	Write-Host "File: $($file)"
-		# # }
-
-		# Write-Host "Start uploading '$($LocalFile)' to '$($RemoteFile)' ..."
-
-		# $content = [System.IO.File]::ReadAllBytes($LocalFile)
-		# $FTPRequest.ContentLength= $content.Length
-		# $writeStream = $FTPRequest.GetRequestStream()
-		# $writeStream.Write($content, 0, $content.Length)
-		# $writeStream.Close()
-		# $writeStream.Dispose()
-		#[System.IO.Stream] uploadStream = FTPHelper.GetUploadStream() 
-		
-		# $Files = Get-ChildItem -Path $this.OutputFolder -Recurse | Select-Obejct FullName
-
-		# Create the target file on the local system and the download buffer 
-		# $LocalFileFile = New-Object IO.FileStream ($LocalFile,[IO.FileMode]::Create) 
-		# [byte[]]$ReadBuffer = New-Object byte[] 1024 
-		# # Loop through the download 
-		# Write-Host "Start uploading '$($LocalFile)' to '$($RemoteFile)' ..."
-		# do { 
-		# 	$ReadLength = $ResponseStream.Read($ReadBuffer,0,1024) 
-		# 	$LocalFileFile.Write($ReadBuffer,0,$ReadLength) 
-		# } 
-		# while ($ReadLength -ne 0)
-		#Write-Host "'$($LocalFile)' was uploaded to '$($RemoteFile)'."
 	}
-	build(){
-		# Write-Host "Start building '$($this.FileToBuild)' output to '$($this.OutputFolder)', clearDest: $($this.ClearDest)"
-		# if($this.IsValidRequest($this.FileToBuild) -ne  $TRUE){
-		# 	Write-Output "'$($this.FileToBuild)' was not existed. Please specify project to build"
-		# 	return
-		# }
-		#$this.OnBeforeBuild()
-		#$this.Building()
+	Build(){
+		$Global:logger.Write("Start building '$($this.FileToBuild)' output to '$($this.OutputFolder)', clearDest: $($this.ClearDest)")
+		if($this.IsValidRequest($this.FileToBuild) -ne  $TRUE){
+			$Global:logger.Write( "'$($this.FileToBuild)' was not existed. Please specify project to build")
+			return
+		}
+		$Global:logger.Write("Preparing ...", "cyan")
+		$this.OnBeforeBuild()
+		$Global:logger.Write("Preparing was completed", "cyan")
+		$Global:logger.Write("Building ...", "cyan")
+		$this.Building()
+		$Global:logger.Write("Building was completed ...", "cyan")
+		$Global:logger.Write("Upload to remote host ...", "cyan")
 		$this.UploadArtifactsToRemote()
+		$Global:logger.Write("Upload was completed...", "cyan")
 		$Global:logger.Write("Building '$($this.FileToBuild)' was completed.")
 	}
 }
@@ -165,12 +120,13 @@ class FTP{
 		[string] $localFile = [PathHelper]::Combine($this.LocalWorkingFolder, $relativePath)
 		[string] $remoteFile = [PathHelper]::Combine($this.BasePath, [PathHelper]::ToFtpPath($relativePath), $Global:FtpFolderSeparator)
 		# need to handle response from server and error
-		$FTPRequest = [System.Net.FtpWebRequest]::Create($remoteFile) 
-		$FTPRequest.Credentials = New-Object System.Net.NetworkCredential($this.UserName, $this.Password) 
-		$FTPRequest.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile 
-		$FTPRequest.UseBinary = $true 
-		$FTPRequest.KeepAlive = $false
-		$FTPRequest.UsePassive = $true
+		[System.Net.FtpWebRequest] $FTPRequest = $this.CreateRequest($remoteFile, [System.Net.WebRequestMethods+Ftp]::UploadFile)
+		# $FTPRequest = [System.Net.FtpWebRequest]::Create($remoteFile) 
+		# $FTPRequest.Credentials = New-Object System.Net.NetworkCredential($this.UserName, $this.Password) 
+		# $FTPRequest.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile 
+		# $FTPRequest.UseBinary = $true 
+		# $FTPRequest.KeepAlive = $false
+		# $FTPRequest.UsePassive = $true
 		$content = [System.IO.File]::ReadAllBytes($localFile)
 		$FTPRequest.ContentLength =$content.Length
 		$writeStream = $FTPRequest.GetRequestStream()
@@ -178,22 +134,6 @@ class FTP{
 		$writeStream.Close()
 		$writeStream.Dispose()
 	}
-	# Upload([string] $localFile, [string] $remoteFile, [string] $userName, [string] $pwd){
-	# 	# need to handle response from server and error
-	# 	$FTPRequest = [System.Net.FtpWebRequest]::Create($remoteFile) 
-	# 	$FTPRequest.Credentials = New-Object System.Net.NetworkCredential($userName, $pwd) 
-	# 	$FTPRequest.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile 
-	# 	$FTPRequest.UseBinary = $true 
-	# 	$FTPRequest.KeepAlive = $false
-	# 	$FTPRequest.UsePassive = $true
-	# 	$content = [System.IO.File]::ReadAllBytes($localFile)
-	# 	$FTPRequest.ContentLength =$content.Length
-	# 	$writeStream = $FTPRequest.GetRequestStream()
-	# 	$writeStream.Write($content, 0, $content.Length)
-	# 	$writeStream.Close()
-	# 	$writeStream.Dispose()
-	# }
-
 	[ActionResult] CreateFolder([string] $path){
 		if([System.String]::IsNullOrWhiteSpace($path)){
 			$Global:logger.Write("invalid '$($path)'")
@@ -204,9 +144,14 @@ class FTP{
 		[string] $createdFolder=""
 
 		For([int] $index=0; $index -lt $paths.Length; $index++){
-			$createdFolder = [PathHelper]::Combine($createdFolder, $paths[$index], $Global:FtpFolderSeparator)
+			if([System.String]::IsNullOrWhiteSpace($createdFolder)){
+				$createdFolder = $paths[$index]
+			}else{
+				$createdFolder = [PathHelper]::Combine($createdFolder, $paths[$index], $Global:FtpFolderSeparator)
+			}
+			
 			[ActionResult] $createFolderResult = $this.CreateFolderByPath($createdFolder)
-			if($createFolderResult.Status -ne [ActionStatusType]::Success){
+			if($createFolderResult.Status -eq [ActionStatusType]::Fail){
 				$result.Status	 = $createFolderResult.Status
 				$result.Message = $createFolderResult.Message
 				Break
@@ -217,26 +162,45 @@ class FTP{
 	}
 	[ActionResult] CreateFolderByPath([string]$path){
 		[ActionResult] $result = [ActionResult]::new([ActionStatusType]::Success)
+		if($this.Exists($path).Status -eq [ActionStatusType]::Exists){
+			$Global:logger.Write("'$($path)' was already existed.")
+			$result.Status=[ActionStatusType]::Exists
+			return $result 
+		}
 		Try{
 			$Global:logger.Write("Creating new '$($path)' folder).")
 			[System.Net.FtpWebRequest] $request = $this.CreateRequest($path, [System.Net.WebRequestMethods+Ftp]::MakeDirectory)
 			[System.Net.FtpWebResponse] $response = [System.Net.FtpWebResponse] $request.GetResponse()
-			[System.IO.Stream] $stream = $response.GetResponseStream()
+			$response.Close()
+			# [System.IO.Stream] $stream = $response.GetResponseStream()
 			$result.Status = [ActionStatusType]::Success
 		}Catch{
 			# Will handle response later
-			#[System.Net.FtpWebResponse] $response = [System.Net.FtpWebResponse] $_.Response
-			$Global:logger.Error($_.Exception.Message)
+			[System.Net.FtpWebResponse] $response = [System.Net.FtpWebResponse] $_.Response
+			$Global:logger.Error("Status code: $($response.StatusCode), Error message: $($_.Exception.Message)")
 			$result.Status=[ActionStatusType]::Fail
 			$result.Message = $_.Exception.Message
 			
 		}
 		return $result
 	}
+	[ActionResult] Exists([string] $path){
+		[ActionResult] $result = [ActionResult]::new([ActionStatusType]::Success)
+		Try{
+			$Global:logger.Write("Check if '$($path)' folder) was existed.")
+			[System.Net.FtpWebRequest] $request = $this.CreateRequest($path, [System.Net.WebRequestMethods+Ftp]::ListDirectory)
+			[System.Net.FtpWebResponse] $response = [System.Net.FtpWebResponse] $request.GetResponse()
+			$response.Close()
+			$result.Status = [ActionStatusType]::Exists
+		}Catch{
+			$result.Status = [ActionStatusType]::Fail
+			$result.Message = $_.Exception.Message
+		}
+		return $result
+	}
 	[System.Net.FtpWebRequest] CreateRequest([string] $path, [string] $action){
 
 		[string] $remoteFile =[PathHelper]::Combine($this.BasePath, $path, $Global:FtpFolderSeparator)
-
 		$Global:logger.Write("Creating request to '$($remoteFile)'")
 		[System.Net.FtpWebRequest]$FTPRequest = [System.Net.FtpWebRequest]::Create($remoteFile) 
 		$FTPRequest.Credentials = New-Object System.Net.NetworkCredential($this.UserName, $this.Password) 
@@ -244,6 +208,7 @@ class FTP{
 		$FTPRequest.UseBinary = $true 
 		$FTPRequest.KeepAlive = $false
 		$FTPRequest.UsePassive = $true
+		#$FTPRequest.Timeout = $Global:FtpTimeout
 		return $FTPRequest
 	}
 }
@@ -279,16 +244,19 @@ Class FileHelper{
 		return ![System.String]::IsNullOrWhiteSpace([System.IO.Path]::GetDirectoryName($path))
 	}
 }
-function buildWebApi([string]$sln, [string]$output, [bool]$clearDest){
+function BuildWebApi([string]$sln, [string]$output, [bool]$clearDest){
 	[BuildAgent] $buildAgent=[BuildAgent]::new($sln, $output, $clearDest)
-	$buildAgent.build()
+	$buildAgent.Build()
 }
 class Logger{
 	Write([string] $str){
-		Write-Host $str
+		$this.Write($str, "white")
+	}
+	Write([string] $str, [string]$color){
+		Write-Host $str -ForegroundColor $color
 	}
 	Error([string] $str){
-		Write-Host $str -ForeGroundColor "Red"
+		$this.Write($str, "red")
 	}
 
 }
@@ -297,4 +265,4 @@ $outputFolder="$outputDir\webapi"
 $cleanFolderBeforeBuild=$TRUE
 $global:logger=[Logger]::new()
 
-buildWebApi $webApiSln $outputFolder $cleanFolderBeforeBuild
+BuildWebApi $webApiSln $outputFolder $cleanFolderBeforeBuild
